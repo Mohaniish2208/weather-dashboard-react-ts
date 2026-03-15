e3d169485e0234e9fa7476a4d7f3045ba80cd13f
Mohaniish2208 <mohaniish5@gmail.com>
2026-03-14 21:00:52 -0400

    Styles updated.

--- vite-project/src/components/weather/WeatherCard/WeatherCard.tsx
+++ vite-project/src/components/weather/WeatherCard/WeatherCard.tsx
@@ -4,7 +4,7 @@ export default function WeatherCard() {
   return (
     <section className="weathercard">
       <h2 className="weathercard-city">Toronto</h2>
-      <p className="weathercard-temperature">12°C</p> {/* alt + 0176 for degree symbol */}
+      <p className="weathercard-temperature">12°C</p>
       <p className="weathercard-condition">Partly Cloudy</p>
       <div className="weathercard-details">
         <div className="weathercard-detail">
