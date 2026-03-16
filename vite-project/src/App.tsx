73a62de293159e706d043cd4cfcf42ce054e4832
Mohaniish2208 <mohaniish5@gmail.com>
2026-03-16 04:04:57 -0400

    App.tsx and index.css updated.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,12 @@
-import Appshell from "./components/layout/Appshell/AppShell"
+import Appshell from "./components/layout/Appshell/AppShell";
+import "./index.css";
 
 function App() {
   return (
     <>
       <Appshell />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
