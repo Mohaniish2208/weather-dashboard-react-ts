14c1f496719d443f4f936957eb16665c0ee990e7
Mohaniish2208 <mohaniish5@gmail.com>
2026-03-17 02:17:01 -0400

    weather.ts updated.

--- vite-project/src/constants/api.ts
+++ vite-project/src/constants/api.ts
@@ -1,7 +1,5 @@
-export const BASE_WEATHER_API =
-  "https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json"
+export const BASE_WEATHER_API = "https://geocoding-api.open-meteo.com/v1/search"
 
-export const WEATHER_FORECAST_API =
-  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+export const WEATHER_FORECAST_API = "https://api.open-meteo.com/v1/forecast"
 
-export const DEFAULT_UNIT = "metrics"
+export const DEFAULT_UNIT = "metric"
--- vite-project/src/types/weather.ts
+++ vite-project/src/types/weather.ts
@@ -1,7 +1,28 @@
 export type GeoCodingResult = {
-  city: string
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+  country?: string
+  admin1?: string
 }
 
-export type LocationResults = {
-  lat: number
+export type GeoCodingResponse = {
+  results?: GeoCodingResponse[]
+}
+
+export type CurrentWeather = {
+  time: number
+  interval: number
+  temperature_2m: number
+  relative_humidity_2m: number
+  apparent_temperature: number
+  weather_code: number
+  wind_speed_10m: number
+}
+
+export type WeatherResponse = {
+  latitude: number
+  longitude: number
+  current: CurrentWeather
 }
